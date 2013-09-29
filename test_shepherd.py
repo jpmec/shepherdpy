d0a260c6d00df6b5d79f041c5bd650fe3561e0dc
Joshua Petitt <joshpetitt@jpmec.com>
2013-09-29 01:21:54 -0500

    Fixed test_shepherd.py to use output from shepherd.run_server

--- test_shepherd.py
+++ test_shepherd.py
@@ -1,14 +1,12 @@
 import shepherd
 import unittest
 import example
-from multiprocessing import Process
-
+from multiprocessing import Pool
 
 
 
 def run_example():
   return shepherd.run_server(example.datasource, example.mapfn, example.reducefn)
-
 
 
 
@@ -18,12 +16,15 @@ class TestSequenceFunctions(unittest.TestCase):
         self.seq = range(10)
 
     def test_example(self):
+
         expected = {'a': 2, 'on': 1, 'great': 1, 'Humpty': 3, 'again': 1, 'wall': 1, 'Dumpty': 2, 'men': 1, 'had': 1, 'all': 1, 'together': 1, "King's": 2, 'horses': 1, 'All': 1, "Couldn't": 1, 'fall': 1, 'and': 1, 'the': 2, 'put': 1, 'sat': 1}
 
-        p = Process(target=run_example)
-        p.start()
+        pool = Pool(processes=1)
+        process = pool.apply_async(run_example)
         shepherd.run_clients()
-        p.join()
+        result = process.get()
+
+        self.assertEqual(expected, result)
 
 
 
