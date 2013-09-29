2fcb37c834925df38ec2e7f6b3dced624471eed8
Joshua Petitt <joshpetitt@jpmec.com>
2013-09-29 11:28:10 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name='shepherd',
+	version='0.0.1',
+	author='Joshua Petitt',
+	py_modules=['shepherd'],
+	scripts=['shepherd.py'],
+	install_requires=[],
+)
